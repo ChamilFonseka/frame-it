b841c174ddb0c213877f0f4690a287e49761435c
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-03-23 20:01:07 +0530

    Add the minimum footer content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <footer className="bg-sky-200">
-            Footer
+        <footer >
+            <div className="container border-t py-6 text-center text-sm text-muted-foreground">
+                <p>© {new Date().getFullYear()} FrameIt. All rights reserved.</p>
+            </div>
         </footer>
     );
 }
